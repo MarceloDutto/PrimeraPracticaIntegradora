87aa1f814dd73019b45e7ec1fcd89f8939ff7ec5
Marcelo Dutto <marcelodutto@hotmail.com>
2023-02-12 22:07:01 -0300

    Se protegieron las variables de entorno

--- src/config/index.js
+++ src/config/index.js
@@ -4,9 +4,9 @@ dotenv.config()
 const config = {
     port: process.env.PORT || 3000,
     db: {
-        userDB: process.env.USER_DB || 'admin',
-        passDB: process.env.PASS_DB || 'coderbackendadmin',
-        hostDB: process.env.HOST_DB || 'coderbackend.ngqatly.mongodb.net'
+        userDB: process.env.USER_DB,
+        passDB: process.env.PASS_DB,
+        hostDB: process.env.HOST_DB
     }
 }
 
